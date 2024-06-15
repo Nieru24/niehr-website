e94d31183d5c8b3f42199221b03b4618caa47e4d
Nieru24 <angelesniel7@gmail.com>
2024-06-15 23:52:42 +0800

    Initial statistic calculator
    jsx and css

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React from 'react';
+import WebsiteBackground from '../components/WebsiteBackground';
+import classes from '../pages-css/NoPage.module.css';
+import { useNavigate } from 'react-router-dom';
 
-function noPage() {
-  const styles ={
-    display: "Grid",
+function NoPage() {
+  const navigate = useNavigate();
+
+  const styles = {
+    display: "flex",
     placeItems: "center",
-    height: "70vh",
-  }
+    minHeight: "80vh",
+    color: "white",
+    justifyContent: "center",
+    backdropFilter: "blur(2px)",
+    WebkitBackdropFilter: "blur(7px) saturate(200%)",
+    borderRadius: "12px",
+    border: "1px solid rgba(209, 213, 219, 0.3)",
+    marginLeft: "20px",
+    marginRight: "20px"
+  };
 
-  return(
-  <>
-  <h1 className="errorMessage" style={styles}>Error 404: Page not found.</h1>          
-  </>
-    );
+  return (
+    <>
+      <WebsiteBackground/>
+      <div className={classes.div}>
+        <button className={classes.button} onClick={() => navigate(-1)}>
+          <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024">
+            <path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"/>
+          </svg>
+          <span>Back</span>
+        </button>
+      </div>
+      <div style={styles}>
+        <h1 className="errorMessage">Error 404: Page not found.</h1>
+      </div>
+    </>
+  );
 }
 
-export default noPage
+export default NoPage;
--- src/pages/StatisticCalculator.jsx
+++ src/pages/StatisticCalculator.jsx
@@ -1,11 +1,117 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import classes from '../pages-css/StatisticCalculator.module.css'
 import Header from '../components/header/Header'
+import WebsiteBackground from '../components/WebsiteBackground'
+
 
 function statisticCalculator() {
+
+  const [myArray, setMyArray] = useState([]);
+  const [previousArray, setPreviousArray] = useState([0]);
+  const [inputValue, setInputValue] = useState('');
+
+  const formatArray = (array) => {
+    const sortedArray = [...array].sort((a, b) => a - b);
+    return sortedArray;
+  };
+
+  // For testing in console
+  useEffect(() => {
+    console.log('myArray:', formatArray(myArray));
+    const sortedArray = formatArray(myArray);
+    console.log(sortedArray[0]);
+  }, [myArray]);
+
+  const handleClear = () => {
+    if (window.confirm('Are you sure you want to clear the array?')) {
+      setPreviousArray(myArray);
+      setMyArray([]);
+    }
+  };
+
+  const handleUndo = () => {
+    if (previousArray.length > 0) {
+      setMyArray(previousArray);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    if (value === '' || /^[0-9]+$/.test(value)) {
+      setInputValue(value);
+    }
+  };
+
+  const handleEnter = () => {
+    if (inputValue !== '') {
+      const newValue = parseInt(inputValue, 10);
+      setPreviousArray(myArray);
+      setMyArray([...myArray, newValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleSampleData = () => {
+    const sampleData = [31, 57, 54, 66, 40, 68, 42, 27, 70, 49, 67, 47, 36, 58, 50, 63, 55, 44, 71, 33, 38, 45, 57, 65, 58, 68, 51, 56, 53, 39, 45, 59, 66, 24, 64, 35, 47, 54, 60, 40, 52, 56, 67, 50, 46];
+    setPreviousArray(myArray);
+    setMyArray(sampleData);
+  };
+
   return (
     <>
-    <Header/>
-    <h1>Calculator Page</h1>
+      <WebsiteBackground/>
+      <Header/>
+      <div className='main-content'>
+        <div className={classes.calculator}>
+          
+          <div className={classes.data}>
+
+            <h1 className={classes.name}>Data</h1>
+
+            <div className={classes.divContainer}>
+              <div className={classes.div1}>
+                <input 
+                  type="number" 
+                  value={inputValue} 
+                  onChange={handleInputChange} 
+                  min="0" 
+                  placeholder="Enter a number" 
+                />
+                <button onClick={handleEnter}>Enter</button>
+                <button onClick={handleSampleData}>Sample Data</button>
+              </div>
+
+              <div className={classes.div2}>
+                <div className={classes.labelsContainer}>
+                  <div className="label">{formatArray(myArray).join(', ')}</div>
+                </div>
+              </div>
+
+              <div className={classes.div3}>
+                  <button onClick={handleClear}>Clear!</button>
+                  <button onClick={handleUndo}>Undo</button>
+              </div>
+            </div>
+
+          </div>
+
+          <div className={classes.ungroupedCalculation}>
+            
+            <h1 className={classes.name}>Ungrouped Data Calculation</h1>
+            
+            <div className={classes.labelsContainer}>
+              <div className={classes.label}>Range:</div>
+              <div className={classes.label}>Mean:</div>
+              <div className={classes.label}>Median:</div>
+              <div className={classes.label}>Mode:</div>
+              <div className={classes.label}>Sample Size (n):</div>
+            </div>
+          </div>
+
+        </div> 
+      </div>
+      
     </>
   )
 }
