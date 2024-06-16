d909e4ea822bdd4430ef48a8222a6c92bcc43200
Nieru24 <angelesniel7@gmail.com>
2024-06-17 05:39:49 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
 import Home from './pages/Home';
 import StatisticCalculator from './pages/StatisticCalculator';
 import About from './pages/About';
 import NoPage from './pages/NoPage';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Router basename="/niehr-website">
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/statistic-calculator" element={<StatisticCalculator />} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/statistic-calculator" element={<StatisticCalculator />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
     </Router>
   );
 }
