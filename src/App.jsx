114692232cf4d458381d85c3b3ccdb0dbdd29f88
Nieru24 <angelesniel7@gmail.com>
2024-06-17 05:44:51 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import StatisticCalculator from './pages/StatisticCalculator';
 import About from './pages/About';
@@ -7,7 +7,7 @@ import NoPage from './pages/NoPage';
 
 function App() {
   return (
-    <Router basename="/niehr-website">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
