a011fd27abe2f7fc74e8ca7e029edea7c8e8c864
Nieru24 <angelesniel7@gmail.com>
2024-06-19 23:27:39 +0800

    More CSS

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import classes from './Header.module.css';
 import { Link } from 'react-router-dom';
 
+
 function Header() {
   return (
     <header className={classes.header}>
       <nav className={classes.nav}>
-        <ul>
-          <li className={classes.logo}>Logo</li>
+        <div className={classes.divcontainer}>
+          <Link to='/home' className={classes.logocontainer}>
+            <div className={classes.logo}></div>
+            <h1><div className={classes.a}>Home</div></h1>
+          </Link>
           <div className={classes.button}>
-            <li className={classes.active}><Link className={classes.a} to='/home'>Home</Link></li>
             <li className={classes.active}><Link className={classes.a} to='/statistic-calculator'>Statistic Calculator</Link></li>
+            <li className={classes.active}><Link className={classes.a} to='/NoPage'>Help</Link></li>
             <li className={classes.active}><Link className={classes.a} to='/about'>About</Link></li>
           </div>
-        </ul>
+        </div>
       </nav>
     </header>
   );
