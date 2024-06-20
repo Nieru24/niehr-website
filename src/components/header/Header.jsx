c10912203758d000827dca3b3cb744790e250f74
Nieru24 <angelesniel7@gmail.com>
2024-06-20 09:56:40 +0800

    Minor adjust

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,8 +14,8 @@ function Header() {
           </Link>
           <div className={classes.button}>
             <li className={classes.active}><Link className={classes.a} to='/statistic-calculator'>Statistic Calculator</Link></li>
-            <li className={classes.active}><Link className={classes.a} to='/NoPage'>Help</Link></li>
             <li className={classes.active}><Link className={classes.a} to='/about'>About</Link></li>
+            <li className={classes.active}><Link className={classes.a} to='/NoPage'>Help</Link></li>
           </div>
         </div>
       </nav>
